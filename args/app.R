54a654e96f33d2baa95ac893fc641019c3609087
debruine <debruine@gmail.com>
2019-06-11 14:35:28 +0100

    function lookup from package::

--- args/app.R
+++ args/app.R
@@ -1,6 +1,6 @@
 library(shiny)
 library(shinydashboard)
-library(magrittr)
+library(rlang)
 
 ui <- dashboardPage(
     dashboardHeader(title = "Arguments"),
@@ -9,16 +9,16 @@ ui <- dashboardPage(
             textInput(inputId = "func", label = "Package::Function", value = "t.test"),
             actionButton(inputId = "goButton", label = "Get Arguments"),
             hr(),
-            tags$a(href="https://github.com/debruine/shiny/tree/master/args/app.R", "Code for this app"),
+            tableOutput("func_table"),
             h3("Available Packages"),
             tableOutput("packages")
         )
     ),
     dashboardBody(
-        p("This app is a demo to test getting the default arguments for a user-input function as inputs."),
+        HTML("<p>This app is a demo to test getting the default arguments for a user-input function as inputs. <a href='https://github.com/debruine/shiny/tree/master/args/app.R'>Code for this app</a></p>"),
         fluidRow(
             box(width = 6, uiOutput("arg_input")),
-            box(width = 6, htmlOutput("args"))
+            box(status = "primary", width = 6, htmlOutput("args"))
         )
     )
 )
@@ -30,7 +30,28 @@ getHTMLhelp <- function(...){
     )
 }
 
+isValidName <- function(string) {
+    grepl("^([[:alpha:]]|[.][._[:alpha:]])[._[:alnum:]]*$", string)
+}
+
 server <- function(input, output) {
+    output$func_table <- renderTable({
+        func_text <- input$func
+        
+        if (grepl("^[._[:alnum:]]+::[._[:alnum:]]*$", func_text)) {
+            split_func <- strsplit(func_text, "::")[[1]]
+            expts <- getNamespaceExports(split_func[1])
+            if (length(split_func) == 1) { split_func[2] <- "" }
+            df <- data.frame(
+                funcs = expts[grep(split_func[2], expts)] %>% sort()
+            )
+            names(df) <- split_func[1]
+            df
+        } else {
+            data.frame()
+        }
+    })
+    
     arglist <- reactive({
         input$goButton
         func_text <- isolate(input$func) %>% 
@@ -38,7 +59,7 @@ server <- function(input, output) {
         func <- parse(text = func_text) %>% eval()
         
         fml <- list()
-        fml[["generic"]] <- formals(func)
+        fml[["main"]] <- formals(func)
         
         m3 <- .S3methods(func)
         for (m in m3) {
@@ -59,7 +80,7 @@ server <- function(input, output) {
     
     output$arg_input <- renderUI({
         arglist <- arglist()
-        lapply(names(arglist), function(a) {
+        tp <- lapply(names(arglist), function(a) {
             args <- arglist[[a]]
             arg_inputs <- lapply(names(args), function(arg) {
                 editable <- TRUE
@@ -67,11 +88,11 @@ server <- function(input, output) {
                     val <- ""
                 } else {
                     val <- tryCatch({
-                            a <- eval(args[[arg]])
-                            if (!is.vector(a) & !is.null(a)) {
-                                a <- capture.output(args[[arg]])
+                            v <- eval(args[[arg]])
+                            if (!is.vector(v) & !is.null(v)) {
+                                v <- capture.output(args[[arg]])
                             }
-                            a
+                            v
                         },
                         error = function(e) {
                             capture.output(args[[arg]])
@@ -90,7 +111,7 @@ server <- function(input, output) {
             })
             
             tabPanel(a, arg_inputs)
-        }) -> tp
+        })
         
         if (length(tp) > 1) {
             do.call(tabsetPanel, tp)
@@ -111,11 +132,25 @@ server <- function(input, output) {
             h <- getHTMLhelp(split_func[2], package = split_func[1])
         }
         
-        start <- grep("<h3>Arguments</h3>", h)
-        table_ends <- grep("</table>", h)
-        end <- table_ends[table_ends>start] %>% min()
+        get_rows <- c()
         
-        paste(h[start:end], collapse = "\n") %>% HTML()
+        startA <- grep("<h3>Arguments</h3>", h)
+        if (length(startA) == 1) {
+            table_ends <- grep("</table>", h)
+            endA <- table_ends[table_ends>startA] %>% min()
+            get_rows <- startA:endA
+        }
+        
+        startD <- grep("<h3>Description</h3>", h) + 1
+        if (length(startD) == 1) {
+            h3_ends <- grep("</h3>", h)
+            endD <- h3_ends[h3_ends>startD] %>% min() - 1
+            get_rows <- c(startD:endD, get_rows)
+        }
+        
+        paste(h[get_rows], collapse = "\n") %>% 
+            paste0("<h2>", func_text, "</h2>\n", .) %>%
+            HTML()
     })
 }
 
