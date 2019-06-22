e203371e0e5000c9d31497a7c43ec47c8106b713
debruine <debruine@gmail.com>
2019-06-22 12:27:24 +0100

    SIPS2019

---
+++ sips2019/app.R
@@ -0,0 +1,105 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+library(XML)
+
+ui <- dashboardPage(
+    dashboardHeader(title = "SIPS 2019 Schedule"),
+    dashboardSidebar(
+        sidebarMenu(
+          tags$a(href="https://osf.io/ndzpt/", "OSF Page"),
+          br(),
+          tags$a(href="https://docs.google.com/spreadsheets/d/1G_SoWUquak6oD-b2L3L-XB7cF97UAGb8PPyVg3r0Lts/edit#gid=0", "Live Updating Schedule")
+        )
+    ),
+    dashboardBody(
+          h2("Sunday"),
+          tabsetPanel(type = "tabs",
+                      tabPanel("13:45", htmlOutput("sunday1")),
+                      tabPanel("14:20", htmlOutput("sunday2")),
+                      tabPanel("15:45", htmlOutput("sunday3")),
+                      tabPanel("16:30", htmlOutput("sunday4"))
+                      ),
+          h2("Monday"),
+          tabsetPanel(type = "tabs",
+                      tabPanel("09:30", htmlOutput("monday1")),
+                      tabPanel("10:15", htmlOutput("monday2")),
+                      tabPanel("11:30", htmlOutput("monday3")),
+                      tabPanel("13:30", htmlOutput("monday4")),
+                      tabPanel("14:45", htmlOutput("monday5")),
+                      tabPanel("15:30", htmlOutput("monday6"))
+                      ),
+          h2("Tuesday"),
+          tabsetPanel(type = "tabs",
+                      tabPanel("09:30", htmlOutput("tuesday1")),
+                      tabPanel("10:15", htmlOutput("tuesday2")),
+                      tabPanel("11:30", htmlOutput("tuesday3")),
+                      tabPanel("13:30", htmlOutput("tuesday4")),
+                      tabPanel("14:45", htmlOutput("tuesday5")),
+                      tabPanel("15:30", htmlOutput("tuesday6"))
+          )
+          )
+)
+
+readGoogleSheet <- function(url){
+  # Suppress warnings because Google docs seems to have incomplete final line
+  suppressWarnings({
+    doc <- paste(readLines(url), collapse=" ")
+  })
+  if(nchar(doc) == 0) stop("No content found")
+  tables <- getNodeSet(htmlParse(doc), "//table")
+  ret <- readHTMLTable(tables[[1]], header=TRUE, 
+                       stringsAsFactors=FALSE, 
+                       as.data.frame=TRUE)
+}
+
+read_row <- function(sips, row) {
+  hm <- sips[row, 2]
+  
+  sessions <- sips[row, 3:26] %>%
+    unlist() %>%
+    unname()
+  
+  sessions[sessions != "" & sessions != "EDGE OF THE WORLD. TURN BACK."] %>%
+    paste0(collapse = "</li>\n\n<li><input type='checkbox'> ") %>%
+    paste0("<h3>", hm, "</h3>\n\n", 
+           "<ul><li><input type='checkbox'> ", ., "</li></ul>")
+}
+
+
+
+server <- function(input, output) {
+  sips <- reactive({
+    # Create a Progress object
+    progress <- shiny::Progress$new()
+    progress$set(message = "Loading Schedule", value = 0)
+    on.exit(progress$close())
+    progress$set(value = 0.5, detail = "running")
+    
+    #googlesheets4::sheets_read("1G_SoWUquak6oD-b2L3L-XB7cF97UAGb8PPyVg3r0Lts", col_names = FALSE)
+    readGoogleSheet(url="https://docs.google.com/spreadsheets/d/1G_SoWUquak6oD-b2L3L-XB7cF97UAGb8PPyVg3r0Lts/edit#gid=0")
+  })
+  
+  output$sunday1 <- renderUI({ read_row(sips(), 17) %>% HTML() })
+  output$sunday2 <- renderUI({ read_row(sips(), 18) %>% HTML() })
+  output$sunday3 <- renderUI({ read_row(sips(), 22) %>% HTML() })
+  output$sunday4 <- renderUI({ read_row(sips(), 23) %>% HTML() })
+  
+  output$monday1 <- renderUI({ read_row(sips(), 42) %>% HTML() })
+  output$monday2 <- renderUI({ read_row(sips(), 43) %>% HTML() })
+  output$monday3 <- renderUI({ read_row(sips(), 45) %>% HTML() })
+  output$monday4 <- renderUI({ read_row(sips(), 47) %>% HTML() })
+  output$monday5 <- renderUI({ read_row(sips(), 49) %>% HTML() })
+  output$monday6 <- renderUI({ read_row(sips(), 50) %>% HTML() })
+  
+  output$tuesday1 <- renderUI({ read_row(sips(), 70) %>% HTML() })
+  output$tuesday2 <- renderUI({ read_row(sips(), 71) %>% HTML() })
+  output$tuesday3 <- renderUI({ read_row(sips(), 73) %>% HTML() })
+  output$tuesday4 <- renderUI({ read_row(sips(), 75) %>% HTML() })
+  output$tuesday5 <- renderUI({ read_row(sips(), 77) %>% HTML() })
+  output$tuesday6 <- renderUI({ read_row(sips(), 78) %>% HTML() })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
