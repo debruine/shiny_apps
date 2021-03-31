b94b1a05722064c8727d83752fcb4dc7bdf2337d
debruine <debruine@gmail.com>
2021-03-31 13:10:54 +0100

    added tails

--- midway_power/app.R
+++ midway_power/app.R
@@ -8,9 +8,12 @@ library(shinydashboard)
 
 midway_tab <- tabItem(
   tabName = "midway_tab",
-  p("Set effect size to 0 to calculate the false positive rate. The minimum cutoff sets the smallest effect size for which you'd continue the study."),
+  p("This app simulates the situation where you run studies and runa preliminary analysis after the first N observations. If the effect size at that point is smaller than the minimum cutoff, you scrap the study. If it is larger, you run a power calculation using this estimated effect size to determine how many more observations to collect to get the specified power for the specified alpha."),
+  p("Set effect size to 0 to calculate the false positive rate."),
+  valueBoxOutput("scrapped"),
+  valueBoxOutput("nn"),
   valueBoxOutput("fpr"),
-  valueBoxOutput("nn")
+  plotOutput("plot")
 )
 
 ## UI ----
@@ -25,13 +28,14 @@ ui <- dashboardPage(
     numericInput("es", "Effect size", 
                  min = 0, max = 1, value = 0, step = .01),
     numericInput("reps", "Simulation reps", 
-                 min = 100, max = 10000, value = 100, step = 100),
+                 min = 100, max = 1000, value = 100, step = 100),
     numericInput("min_cutoff", "Minimum cutoff", 
                  min = 0, max = 3.0, value = .1, step = .01),
     numericInput("alpha", "Alpha", 
                  min = 0.001, max = 1.0, value = .05, step = .001),
     numericInput("power", "Power", 
-                 min = 0.1, max = 1.0, value = .8, step = .05)
+                 min = 0.1, max = 1.0, value = .8, step = .05),
+    radioButtons("alternative", "Tails", c("one.sided", "two.sided"))
   ),
   dashboardBody(
     tabItems(
@@ -42,43 +46,87 @@ ui <- dashboardPage(
 
 ## server ----
 server <- function(input, output, session) {
-
-  output$fpr <- renderValueBox({
-    fp <- replicate(input$reps, {
+  dat <- reactive({
+    purrr::map_dfr(1:input$reps, ~{
+      # simulate data from population with specific effect size
       dat <- rnorm(input$n_first, input$es)
+      
+      # get observed effect size
       obs_d <- mean(dat)/sd(dat)
-      if (obs_d >= input$min_cutoff) {
+      if (input$alternative == "two.sided") {
+        obs_d2 <- abs(obs_d)
+      } else {
+        obs_d2 <- obs_d
+      }
+      
+      # only if observed ES > cutoff
+      if (obs_d2 >= input$min_cutoff) {
+        # get required N to power observed ES
         power <- power.t.test(
-          delta = obs_d, 
+          delta = obs_d2, 
           sig.level = 0.05, 
           power = input$power, 
-          type = "one.sample"
+          type = "one.sample",
+          alternative = input$alternative
         )
        
+        # calculate how many more subjects needed to power for observed ES
         additional_n <- power$n - input$n_first 
         if (additional_n < 0) additional_n = 0
         
+        # add additional subjects to first subjects and get p-values of result
         p <- replicate(input$reps, {
-          dat <- c(dat, rnorm(additional_n))
-          t.test(dat)$p.value
+          dat <- c(dat, rnorm(additional_n, input$es))
+          alt <- input$alternative
+          if (alt == "one.sided") {
+            alt <- ifelse(obs_d < 0, "less", "greater")
+          }
+          t.test(dat, alternative = alt)$p.value
         })
         
-        paste(mean(p < input$alpha), power$n)
+        # return 
+        list(
+          scrapped = "no",
+          obs_d = obs_d,
+          power = mean(p < input$alpha), 
+          n = power$n
+        )
       } else {
         # give up is observed effect size less than cutoff
-        NA
+        list(
+          scrapped = "yes",
+          obs_d = obs_d,
+          power = NA,
+          n = NA
+        )
       }
     })
+  })
     
-    fp2 <- fp[!is.na(fp)]
-    a <- as.data.frame(t(as.data.frame(strsplit(fp2, " "), stringsAsFactors = FALSE)), stringsAsFactors = FALSE)
-    
-    nn <- round(mean(as.numeric(a$V2), na.rm = TRUE))
-    output$nn <- renderValueBox({ valueBox(nn, "Mean N", color = "red") })
-    
-    fpr <- round(mean(as.numeric(a[["V1"]]), na.rm = TRUE), 2)
+
+  output$nn <- renderValueBox({
+    nn <- round(mean(dat()$n, na.rm = TRUE))
+    valueBox(nn, "Mean Total N", color = "purple") 
+  })
+  
+  output$fpr <- renderValueBox({
+    fpr <- round(mean(dat()$power, na.rm = TRUE), 2)
     label <- ifelse(input$es == 0, "False Positive Rate", "Power")
-    valueBox(fpr, label, color = "red")
+    valueBox(fpr, label, color = "green") 
+  })
+  
+  output$scrapped <- renderValueBox({
+    scrapped <- round(100*mean(dat()$scrapped == "yes"))
+    valueBox(scrapped, "Percent studies scrapped", color = "blue") 
+  })
+  
+  output$plot <- renderPlot({
+    ggplot(dat(), aes(x = obs_d, fill = scrapped)) +
+      geom_histogram(binwidth = 0.05, color = "grey") +
+      geom_vline(xintercept = input$min_cutoff) +
+      scale_fill_manual(values = c("red", "#0073B7")) +
+      xlab(paste0("Observed effect size after first ", 
+                  input$n_first, " observations"))
   })
   
 } # end server()
