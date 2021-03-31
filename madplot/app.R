bf758e326a2a608a755231aa8e662a12b1eaee2f
debruine <debruine@gmail.com>
2021-03-31 12:22:08 +0100

    data only changes on request now

--- madplot/app.R
+++ madplot/app.R
@@ -8,10 +8,11 @@ library(dplyr) # for data wrangling
 ui <- dashboardPage(
   dashboardHeader(title = "MadPlots"),
   dashboardSidebar(
-    textInput("xlab",  "X-axis"),
-    textInput("x1",  "X category 1"),
-    textInput("x2",  "X category 2"),
-    textInput("ylab",  "Y-axis")
+    textInput("xlab",  "X-axis", "X"),
+    textInput("x1",  "X category 1", "A"),
+    textInput("x2",  "X category 2", "B"),
+    textInput("ylab",  "Y-axis", "Y"),
+    actionButton("resim", "Re-Simulate Data")
   ),
   dashboardBody(
     plotOutput("madPlot")
@@ -21,19 +22,23 @@ ui <- dashboardPage(
 
 # Define server logic ----
 server <- function(input, output, session) {
-  output$madPlot <- renderPlot({
-    dat <- data.frame(
-      x = rep(c(input$x1, input$x2), each = 100),
+  rv <- reactiveValues()
+  
+  observeEvent(input$resim, {
+    rv$dat <- data.frame(
+      x = rep(c("A", "B"), each = 100),
       y = c(rnorm(100), rnorm(100, runif(1)))
     )
-      
-    thePlot <- ggplot(dat, aes(x, y)) +
+  }, ignoreNULL = FALSE)
+  
+  output$madPlot <- renderPlot({
+    ggplot(rv$dat , aes(x, y)) +
       geom_violin(aes(fill = x), show.legend = FALSE) +
       geom_boxplot(width = 0.25) +
       xlab(input$xlab) +
-      ylab(input$ylab)
-    
-    thePlot
+      ylab(input$ylab) +
+      scale_x_discrete(labels = c(input$x1, input$x2)) +
+      theme_minimal(base_size = 18)
   })
 }
 
